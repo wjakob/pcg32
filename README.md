f7b0dbd104d329e7abf5bc890df9a842cd7d7ad6
Wenzel Jakob <wenzel@inf.ethz.ch>
2015-02-13 15:18:28 +0100

    Added distance computation support, ported example application, misc. cleanups

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # pcg32
-This is a tiny self-contained C++ immplementation of the PCG32 random number generator based on code available at http://www.pcg-random.org.
+This is a tiny self-contained C++ implementation of the PCG32 random number
+based on code by Melissa O'Neill available at http://www.pcg-random.org.
 
-I decide to make my own version since the official small version lacks a C++ interface and various features (e.g. rewind support and floating point sample generation), and the big C++ version is extremely large and uses very recent C++ features that are not yet supported by all compilers.
+I decided to make put together my own version because the official small
+implementation lacks a C++ interface and various important features (e.g.
+rewind/difference support, shuffling, floating point sample generation), and
+the big C++ version is extremely large and uses very recent language features
+that are not yet supported by all compilers.
--- pcg32.h
+++ pcg32.h
@@ -1,6 +1,7 @@
 /*
  * Tiny self-contained version of the PCG Random Number Generation for C++
- * put together from pieces of the much larger C codebase by Wenzel Jakob.
+ * put together from pieces of the much larger C/C++ codebase.
+ * Wenzel Jakob, February 2015
  *
  * The PCG random number generator was developed by Melissa O'Neill <oneill@pcg-random.org>
  *
@@ -22,127 +23,159 @@
  *     http://www.pcg-random.org
  */
 
-#ifndef PCG32_H_INCLUDED
-#define PCG32_H_INCLUDED 1
+#ifndef __PCG32_H
+#define __PCG32_H 1
+
+#define PCG32_DEFAULT_STATE  0x853c49e6748fea9bULL
+#define PCG32_DEFAULT_STREAM 0xda3e39cb94b95bdbULL
+#define PCG32_MULT           0x5851f42d4c957f2dULL
 
 #include <inttypes.h>
 #include <cmath>
+#include <cassert>
 
 /// PCG32 Pseudorandom number generator
 struct pcg32 {
-	/// Initialize the pseudorandom number generator with default seed
-	inline pcg32(uint64_t state = 0x853c49e6748fea9bULL,
-	             uint64_t inc = 0xda3e39cb94b95bdbULL) : state(state), inc(inc) { }
+    /// Initialize the pseudorandom number generator with default seed
+    pcg32() : state(PCG32_DEFAULT_STATE), inc(PCG32_DEFAULT_STREAM) {}
 
-	/**
-	 * \brief Seed the pseudorandom number generator
-	 *
-	 * Specified in two parts: a state initializer and a sequence selection
-	 * constant (a.k.a. stream id)
-	 */
-	inline void seed(uint64_t initstate, uint64_t initseq = 1) {
-		state = 0U;
-		inc = (initseq << 1u) | 1u;
-		nextUInt();
-		state += initstate;
-		nextUInt();
-	}
+    /// Initialize the pseudorandom number generator with the \ref seed() function
+    pcg32(uint64_t initstate, uint64_t initseq = 1u) { seed(initstate, initseq); }
 
-	/// Generate a uniformly distributed unsigned 32-bit random number
-	inline uint32_t nextUInt() {
-		uint64_t oldstate = state;
-		state = oldstate * 6364136223846793005ULL + inc;
-		uint32_t xorshifted = (uint32_t) (((oldstate >> 18u) ^ oldstate) >> 27u);
-		uint32_t rot = (uint32_t) (oldstate >> 59u);
-		return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
-	}
+    /**
+     * \brief Seed the pseudorandom number generator
+     *
+     * Specified in two parts: a state initializer and a sequence selection
+     * constant (a.k.a. stream id)
+     */
+    void seed(uint64_t initstate, uint64_t initseq = 1) {
+        state = 0U;
+        inc = (initseq << 1u) | 1u;
+        nextUInt();
+        state += initstate;
+        nextUInt();
+    }
 
-	/// Generate a uniformly distributed number, r, where 0 <= r < bound
-	inline uint32_t nextUInt(uint32_t bound) {
-		// To avoid bias, we need to make the range of the RNG a multiple of
-		// bound, which we do by dropping output less than a threshold.
-		// A naive scheme to calculate the threshold would be to do
-		//
-		//     uint32_t threshold = 0x100000000ull % bound;
-		//
-		// but 64-bit div/mod is slower than 32-bit div/mod (especially on
-		// 32-bit platforms).  In essence, we do
-		//
-		//     uint32_t threshold = (0x100000000ull-bound) % bound;
-		//
-		// because this version will calculate the same modulus, but the LHS
-		// value is less than 2^32.
+    /// Generate a uniformly distributed unsigned 32-bit random number
+    uint32_t nextUInt() {
+        uint64_t oldstate = state;
+        state = oldstate * PCG32_MULT + inc;
+        uint32_t xorshifted = (uint32_t) (((oldstate >> 18u) ^ oldstate) >> 27u);
+        uint32_t rot = (uint32_t) (oldstate >> 59u);
+        return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+    }
 
-		uint32_t threshold = -bound % bound;
+    /// Generate a uniformly distributed number, r, where 0 <= r < bound
+    uint32_t nextUInt(uint32_t bound) {
+        // To avoid bias, we need to make the range of the RNG a multiple of
+        // bound, which we do by dropping output less than a threshold.
+        // A naive scheme to calculate the threshold would be to do
+        //
+        //     uint32_t threshold = 0x100000000ull % bound;
+        //
+        // but 64-bit div/mod is slower than 32-bit div/mod (especially on
+        // 32-bit platforms).  In essence, we do
+        //
+        //     uint32_t threshold = (0x100000000ull-bound) % bound;
+        //
+        // because this version will calculate the same modulus, but the LHS
+        // value is less than 2^32.
 
-		// Uniformity guarantees that this loop will terminate.  In practice, it
-		// should usually terminate quickly; on average (assuming all bounds are
-		// equally likely), 82.25% of the time, we can expect it to require just
-		// one iteration.  In the worst case, someone passes a bound of 2^31 + 1
-		// (i.e., 2147483649), which invalidates almost 50% of the range.  In 
-		// practice, bounds are typically small and only a tiny amount of the range
-		// is eliminated.
-		for (;;) {
-			uint32_t r = nextUInt();
-			if (r >= threshold)
-				return r % bound;
-		}
-	}
+        uint32_t threshold = -bound % bound;
 
-	/// Generate a single precision floating point value on the interval [0, 1)
-	inline float nextFloat() {
-		return std::ldexp((float) nextUInt(), -32);
-	}
+        // Uniformity guarantees that this loop will terminate.  In practice, it
+        // should usually terminate quickly; on average (assuming all bounds are
+        // equally likely), 82.25% of the time, we can expect it to require just
+        // one iteration.  In the worst case, someone passes a bound of 2^31 + 1
+        // (i.e., 2147483649), which invalidates almost 50% of the range.  In
+        // practice, bounds are typically small and only a tiny amount of the range
+        // is eliminated.
+        for (;;) {
+            uint32_t r = nextUInt();
+            if (r >= threshold)
+                return r % bound;
+        }
+    }
 
-	/// Generate a double precision floating point value on the interval [0, 1)
-	inline double nextDouble() {
-		return std::ldexp((double) nextUInt(), -32);
-	}
+    /// Generate a single precision floating point value on the interval [0, 1)
+    float nextFloat() {
+        return std::ldexp((float) nextUInt(), -32);
+    }
 
-	/// Multi-step advance function (jump-ahead, jump-back)
-	inline void advance(int64_t delta) {
-		state = pcg_advance_lcg_64(state, (uint64_t) delta,
-			6364136223846793005ULL, inc);
-	}
+    /// Generate a double precision floating point value on the interval [0, 1)
+    double nextDouble() {
+        return std::ldexp((double) nextUInt(), -32);
+    }
 
-	/**
-	 * Draw uniformly distributed permutation and permute the
-	 * given STL container (Knuth, TAoCP Vol. 2 (3rd 3d), Section 3.4.2)
-	 */
-	template <typename Iterator> inline void shuffle(Iterator begin, Iterator end) {
-		for (Iterator it = end - 1; it > begin; --it)
-			std::iter_swap(it, begin + nextUInt((uint32_t) (it - begin + 1)));
-	}
+    /**
+     * \brief Multi-step advance function (jump-ahead, jump-back)
+     *
+     * The method used here is based on Brown, "Random Number Generation
+     * with Arbitrary Stride", Transactions of the American Nuclear
+     * Society (Nov. 1994). The algorithm is very similar to fast
+     * exponentiation.
+     */
+    void advance(int64_t delta_) {
+        uint64_t
+            cur_mult = PCG32_MULT,
+            cur_plus = inc,
+            acc_mult = 1u,
+            acc_plus = 0u;
 
-	uint64_t state;  // RNG state.  All values are possible.
-	uint64_t inc;    // Controls which RNG sequence (stream) is selected. Must *always* be odd.
-private:
+        /* Even though delta is an unsigned integer, we can pass a signed
+           integer to go backwards, it just goes "the long way round". */
+        uint64_t delta = (uint64_t) delta_;
 
-	/**
-	 * Multi-step advance functions (jump-ahead, jump-back)
-	 *
-	 * The method used here is based on Brown, "Random Number Generation
-	 * with Arbitrary Stride,", Transactions of the American Nuclear
-	 * Society (Nov. 1994).  The algorithm is very similar to fast
-	 * exponentiation.
-	 *
-	 * Even though delta is an unsigned integer, we can pass a
-	 * signed integer to go backwards, it just goes "the long way round".
-	 */
-	static uint64_t pcg_advance_lcg_64(uint64_t state, uint64_t delta, uint64_t cur_mult, uint64_t cur_plus) {
-		uint64_t acc_mult = 1u;
-		uint64_t acc_plus = 0u;
-		while (delta > 0) {
-			if (delta & 1) {
-				acc_mult *= cur_mult;
-				acc_plus = acc_plus * cur_mult + cur_plus;
-			}
-			cur_plus = (cur_mult + 1) * cur_plus;
-			cur_mult *= cur_mult;
-			delta /= 2;
-		}
-		return acc_mult * state + acc_plus;
-	}
+        while (delta > 0) {
+            if (delta & 1) {
+                acc_mult *= cur_mult;
+                acc_plus = acc_plus * cur_mult + cur_plus;
+            }
+            cur_plus = (cur_mult + 1) * cur_plus;
+            cur_mult *= cur_mult;
+            delta /= 2;
+        }
+        state = acc_mult * state + acc_plus;
+    }
+
+    /**
+     * \brief Draw uniformly distributed permutation and permute the
+     * given STL container
+     *
+     * From: Knuth, TAoCP Vol. 2 (3rd 3d), Section 3.4.2
+     */
+    template <typename Iterator> void shuffle(Iterator begin, Iterator end) {
+        for (Iterator it = end - 1; it > begin; --it)
+            std::iter_swap(it, begin + nextUInt((uint32_t) (it - begin + 1)));
+    }
+
+    /// Compute the distance between two PCG32 pseudorandom number generators
+    int64_t operator-(const pcg32 &other) const {
+        assert(inc == other.inc);
+
+        uint64_t
+            cur_mult = PCG32_MULT,
+            cur_plus = inc,
+            cur_state = other.state,
+            the_bit = 1u,
+            distance = 0u;
+
+        while (state != cur_state) {
+            if ((state & the_bit) != (cur_state & the_bit)) {
+                cur_state = cur_state * cur_mult + cur_plus;
+                distance |= the_bit;
+            }
+            assert((state & the_bit) == (cur_state & the_bit));
+            the_bit <<= 1;
+            cur_plus = (cur_mult + 1ULL) * cur_plus;
+            cur_mult *= cur_mult;
+        }
+
+        return (int64_t) distance;
+    }
+
+    uint64_t state;  // RNG state.  All values are possible.
+    uint64_t inc;    // Controls which RNG sequence (stream) is selected. Must *always* be odd.
 };
 
-#endif // PCG32_H_INCLUDED
+#endif // __PCG32_H
